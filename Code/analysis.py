e1781fad8a22bebe2225c9326ce9806621997520
skundal <skundal@ncsu.edu>
2019-10-18 16:55:22 -0400

    update analysis.py- stat test: normality completed

--- Code/analysis.py
+++ Code/analysis.py
@@ -1,12 +1,15 @@
 import pandas as pd
 from scipy.stats import shapiro
+from scipy.stats import normaltest
+from scipy.stats import anderson
 from tabulate import tabulate
 from datetime import datetime
+
 
 def analysis(path, target):
     # create file
     now = datetime.now()
-    dt_string = "analysis_"+now.strftime("%d_%m_%Y_%H_%M_%S") + ".txt"
+    dt_string = "analysis_" + now.strftime("%d_%m_%Y_%H_%M_%S") + ".txt"
     f = open(dt_string, "w")
     data = pd.read_csv(path, sep=',', header=0)
     columns = list(data.columns)
@@ -21,28 +24,72 @@ def analysis(path, target):
     # Correlation
     f.writelines("\n\nCorrelation:\n")
     correlations = data[data.columns].corr(method='pearson')
-    for i in range(len(columns)-1):
-        for j in range(i+1, len(columns)):
-            if abs(correlations[columns[i]][columns[j]])<0.5:
-              f.writelines("\nColumns "+columns[i]+" and "+columns[j] +" have low correlation between them. The correlation value is "+str(correlations[columns[i]][columns[j]]))
-            elif abs(correlations[columns[i]][columns[j]])>0.98:
-              f.writelines("\nColumns "+columns[i]+" and "+columns[j] +" have very high correlation between them. The correlation value is "+str(correlations[columns[i]][columns[j]]))
+    for i in range(len(columns) - 1):
+        for j in range(i + 1, len(columns)):
+            if abs(correlations[columns[i]][columns[j]]) < 0.5:
+                f.writelines("\nColumns " + columns[i] + " and " + columns[
+                    j] + " have low correlation between them. The correlation value is " + str(
+                    correlations[columns[i]][columns[j]]))
+            elif abs(correlations[columns[i]][columns[j]]) > 0.98:
+                f.writelines("\nColumns " + columns[i] + " and " + columns[
+                    j] + " have very high correlation between them. The correlation value is " + str(
+                    correlations[columns[i]][columns[j]]))
             else:
-              f.writelines("\nColumns "+columns[i]+" and "+columns[j] +" have strong correlation between them. The correlation value is "+str(correlations[columns[i]][columns[j]]))
-    
-    # STATISTICS TEST
-    # Normal Distribution Test
-    f.writelines("\n\nShapiro-Wilk test - normal distribution test\n")
-    ls = []
+                f.writelines("\nColumns " + columns[i] + " and " + columns[
+                    j] + " have strong correlation between them. The correlation value is " + str(
+                    correlations[columns[i]][columns[j]]))
 
+    # STATISTICS TEST
+    # Normality Test
+    f.writelines("\n\nSTATISTICS TEST:\n\nNormality Tests:")
+    # Shapiro-Wilk test
+    f.writelines("\nShapiro-Wilk test - Gaussian distribution test\n")
+    f.writelines("Tests whether a data sample has a Gaussian distribution.\n")
+    f.writelines("Hypothesis: the sample has a Gaussian distribution\n")
+    ls = []
     for i in columns:
         if i == target:
             continue
         stat, p = shapiro(data[i])
-        if p>0.05:
+        if p > 0.05:
             result = "Accepted"
         else:
             result = "Rejected"
-        ls.append([i,stat,p,result])
-    dataf = pd.DataFrame(ls, columns = ["Column","Test Statistics","p-Value","Null Hypothesis"])
+        ls.append([i, stat, p, result])
+    dataf = pd.DataFrame(ls, columns=["Column", "Test Statistics", "p-Value", "Null Hypothesis"])
     f.write(tabulate(dataf, tablefmt="grid", headers="keys", showindex=False))
+
+    # D’Agostino’s K^2 test
+    f.writelines("\n\nD’Agostino’s K^2 Test - Gaussian distribution test\n")
+    f.writelines("Tests whether a data sample has a Gaussian distribution.\n")
+    f.writelines("Hypothesis: the sample has a Gaussian distribution\n")
+    ls = []
+    for i in columns:
+        if i == target:
+            continue
+        stat, p = shapiro(data[i])
+        if p > 0.05:
+            result = "Accepted"
+        else:
+            result = "Rejected"
+        ls.append([i, stat, p, result])
+    dataf = pd.DataFrame(ls, columns=["Column", "Test Statistics", "p-Value", "Null Hypothesis"])
+    f.write(tabulate(dataf, tablefmt="grid", headers="keys", showindex=False))
+
+    # Anderson-Darling Test
+    f.writelines("\n\nAnderson-Darling Test - Gaussian distribution test\n")
+    f.writelines("Tests whether a data sample has a Gaussian distribution.\n")
+    f.writelines("Hypothesis: the sample has a Gaussian distribution\n")
+    ls = []
+    for i in columns:
+        if i == target:
+            continue
+        result = anderson(data[i])
+        f.writelines('\n'+i+':\nStatistic: '+ str(result.statistic)+'\n')
+        p = 0
+        for j in range(len(result.critical_values)):
+            sl, cv = result.significance_level[j], result.critical_values[j]
+            if result.statistic < result.critical_values[j]:
+                f.writelines(str(sl)+':'+str(cv) +' Null Hypothesis - Accepted\n')
+            else:
+                f.writelines(str(sl)+':'+str(cv) +' Null Hypothesis - Rejected\n')
