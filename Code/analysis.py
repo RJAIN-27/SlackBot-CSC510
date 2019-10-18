32467721e322499ee72b0fb0a1e529cae8fea424
skundal <skundal@ncsu.edu>
2019-10-18 16:25:59 -0400

    Update analysis.py- added EDA

--- Code/analysis.py
+++ Code/analysis.py
@@ -8,17 +8,33 @@ def analysis(path, target):
     now = datetime.now()
     dt_string = "analysis_"+now.strftime("%d_%m_%Y_%H_%M_%S") + ".txt"
     f = open(dt_string, "w")
+    data = pd.read_csv(path, sep=',', header=0)
+    columns = list(data.columns)
 
     # Exploratory data analysis:
-    f.writelines("EXPLORATORY DATA ANALYSIS")
-    f.writelines("\nCorrelation")
+    f.writelines("\nEXPLORATORY DATA ANALYSIS:")
 
+    # number of null values per column
+    f.writelines("\n\nNo. of nulls in the columns:\n")
+    f.write(str(data.isnull().sum()))
 
-    data = pd.read_csv(path, sep=',', header=0)
-
-    f.writelines("\nShapiro-Wilk test - normal distribution test\n")
+    # Correlation
+    f.writelines("\n\nCorrelation:\n")
+    correlations = data[data.columns].corr(method='pearson')
+    for i in range(len(columns)-1):
+        for j in range(i+1, len(columns)):
+            if abs(correlations[columns[i]][columns[j]])<0.5:
+              f.writelines("\nColumns "+columns[i]+" and "+columns[j] +" have low correlation between them. The correlation value is "+str(correlations[columns[i]][columns[j]]))
+            elif abs(correlations[columns[i]][columns[j]])>0.98:
+              f.writelines("\nColumns "+columns[i]+" and "+columns[j] +" have very high correlation between them. The correlation value is "+str(correlations[columns[i]][columns[j]]))
+            else:
+              f.writelines("\nColumns "+columns[i]+" and "+columns[j] +" have strong correlation between them. The correlation value is "+str(correlations[columns[i]][columns[j]]))
+    
+    # STATISTICS TEST
+    # Normal Distribution Test
+    f.writelines("\n\nShapiro-Wilk test - normal distribution test\n")
     ls = []
-    columns = list(data.columns)
+
     for i in columns:
         if i == target:
             continue
