ebb96cb2db8bb3e9a72ab099ba04873ecbc162a2
rjain27 <rjain27@ncsu.edu>
2019-10-19 17:25:55 -0400

    before adding the alternate flows - integrated

--- Code/command.py
+++ Code/command.py
@@ -1,4 +1,9 @@
 import kwx
+import modelSelection
+import analysis
+
+path = "my.csv"
+flag=0
 
 class Command(object):
  
@@ -7,3 +12,23 @@ class Command(object):
             list=kwx.keywordExtraction(command)
             #list = command.split(' ')
             return list
+
+        elif flag==1:
+            analyses_file=analysis.analysis(path, command)
+            return analyses_file
+        else:
+            model_Selection=modelSelection.modelSelection(path, command)   
+            return model_Selection
+
+    def handlecommands(self, user, command):
+        global flag
+        if "suggestion" in command or "selection" in command:
+            flag=0
+            response="Please provide the target column"
+            #list=modelSelection.modelSelection(file, target)
+            return response
+
+        if "analyze" in command or "analyze" in command:
+            response="Please provide the target"
+            flag=1
+            return response
