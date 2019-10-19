bfb7d67b39c76327b1a1988ec93abbe8d309abb7
rjain27 <rjain27@ncsu.edu>
2019-10-19 18:53:31 -0400

    after integration with alternate flow

--- Code/event.py
+++ Code/event.py
@@ -24,17 +24,15 @@ class Event:
         if event and 'files' in event and 'text' in event and event['user']!="UP6FMPQ1X" and event['upload']==True:
             print(event['files'][0]['filetype'])   
             print(event['files'][0]['url_private'])
-            response = requests.get(event['files'][0]['url_private'], headers={'Authorization': 'Bearer xoxb-795814705207-788531806065-9dWeyIRqj2t1LSbICYnDkB01'})
-            with open("my.csv",'wb') as f: 
-                f.write(response.content) 
-            f.close()    
-            #f1=open("analysis_19_10_2019_14_07_54.txt", "r")
-            #content=f1.read()
-            #print(content)
-            #print(type(content))
-            #self.bot.slack_client.api_call("files.upload", channels=event['channel'], file=content)
-           
-            self.handleevent1(event['user'], event['text'], event['channel'])    
+            if(event['files'][0]['filetype'] == "csv"):
+                response = requests.get(event['files'][0]['url_private'], headers={'Authorization': 'Bearer xoxb-795814705207-788531806065-9dWeyIRqj2t1LSbICYnDkB01'})
+                with open("my.csv",'wb') as f: 
+                    f.write(response.content) 
+                f.close()    
+                self.handleevent1(event['user'], event['text'], event['channel']) 
+            else:
+                self.bot.slack_client.api_call("chat.postMessage", channel=event['channel'], text="I am sorry can you please give me a csv file\n", as_user=True)
+
      
     def handleevent(self, user, command, channel):
         
@@ -42,8 +40,6 @@ class Event:
             print ("Received command: " + command + " in channel: " + channel + " from user: " + user)
             response = self.command.handlecommand(user, command)
             print(response)
-  
-            self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="The information you asked is:\n", as_user=True)
 
             if type(response) is str:
                 
@@ -51,16 +47,28 @@ class Event:
                     f1=open(response, "r")
                     content=f1.read()
                     print(content)
-                    print(type(content))
+                    f1.close()
+                    #print(type(content))
+                    self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="The information you asked is:\n", as_user=True)
                     self.bot.slack_client.api_call("files.upload", channels=channel, file=content)
-                else:    
+                    self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="Were you satisfied with the analysis?", as_user=True)
+                elif(response == "The target column is not present in the file. Please upload the file again and give the correct target column name. Remember, target column is case sensitive."):
                     self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+                elif(response == "Sorry about that :("):
+                    self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+                elif(response == "Thankyou for the feedback"):
+                    self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+                else:  
+                    self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="The information you asked is:\n", as_user=True)  
+                    self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+                    self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="Were you satisfied with the recommendation?", as_user=True)
 
             if type(response) is list:
                 for i in response:
                     for j in i:
                         self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=i[j], as_user=True)
-
+                self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="Were you satisfied with the details?", as_user=True)
+            
 
         #    f1=open(response, "r")
         #    content=f1.read()
@@ -82,5 +90,3 @@ class Event:
             response = self.command.handlecommands(user, command)   
             self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
             
-
-
