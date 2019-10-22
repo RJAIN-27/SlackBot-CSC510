f7a9fdac0ecebc22ebf824d4aded753867e98627
skundal <skundal@ncsu.edu>
2019-10-22 19:46:38 -0400

    Update mocking_infrastructure.py

--- Code/mocking_infrastructure.py
+++ Code/mocking_infrastructure.py
@@ -1,67 +1,74 @@
 import json
 import operator
 import openpyxl as xl
+import requests
+from mock import Mock
 
-with open("data.json") as json_file:
-    data = json.load(json_file)
-columnNames = data["columnNames"]
-modelDict = data["listModels"]
-wrngColEx = data["wrongTargetColumnException"]
-parameters_count=int(data["parameters_to_be_counted"])
-target=data["target"]
-wrong_target=data["wrong_target"]
-path=data["path_to_csv_file_case1_case2"]
-libraries = data["libraries"]
+
+def GET(url):
+    if url=="http://localhost:3000/":
+        global data, columnNames, modelDict, wrngColEx, parameters_count, target, wrong_target, path, libraries
+        with open("data.json") as json_file:
+            data = json.load(json_file)
+        return data
 
 # USECASE 1
-def mockbestModel(path,target):
+def mockbestModel(path, target):
+    requests.get = Mock(side_effect=GET)
+    data = requests.get(url = "http://localhost:3000/")
+    columnNames = data["columnNames"]
+    modelDict = data["listModels"]
     # for mocking the main and alternate flow
-    flag = targetCheck(target,columnNames)
-    if flag !=1:
+    flag = targetCheck(target, columnNames)
+    if flag != 1:
         return flag
     sorted_x = sorted(modelDict.items(), key=operator.itemgetter(1))
-    return str(sorted_x[len(sorted_x)-1][0])
-
-def max_val_fun():
-    ls = []
-    for i in modelDict:
-        ls.append(modelDict[i])
-    return max(ls)
+    return str(sorted_x[len(sorted_x) - 1][0])
 
 def targetCheck(target, columnNames):
     return 1 if target in columnNames else "The target column is not present in the file. Please upload the file again and give the correct target column name. Remember, target column is case sensitive."
 
+
 # USECASE 2
 def mock_analysis_interaction(path, target):
-    flag = targetCheck(target,columnNames)
-    if flag !=1:
+    requests.get = Mock(side_effect=GET)
+    data = requests.get(url="http://localhost:3000/")
+    columnNames = data["columnNames"]
+    parameters_count = int(data["parameters_to_be_counted"])
+    flag = targetCheck(target, columnNames)
+    if flag != 1:
         return flag
-    filename=data["path_for_usecase2"]
-    a=""
-    count=0
+    filename = data["path_for_usecase2"]
+    a = ""
+    count = 0
     for line in open(filename, 'r'):
-        a=a+line
+        a = a + line
     if "MEAN" in a:
-        count=count+1
+        count = count + 1
     if "MEDIAN" in a:
-        count=count+1
+        count = count + 1
     if "MODE" in a:
-        count=count+1
+        count = count + 1
     if "Correlation" in a:
-        count=count+1
+        count = count + 1
     if "Normality Tests" in a:
-        count=count+1
+        count = count + 1
     if count == parameters_count:
         return str(filename)
     return 0
 
+
 # USECASE 3
 def mock_keyword_extraction(a):
+    requests.get = Mock(side_effect=GET)
+    data = requests.get(url="http://localhost:3000/")
+    libraries = data["libraries"]
+
     list1 = str(a).split(' ')
     ans_list = []
     length = len(list1)
     i = 0
-    wb = xl.load_workbook('libraryFile.xlsx')
+    wb = xl.load_workbook(data["xlsx_file"])
     sheet = wb['Sheet1']
     libInfo = {}
     for row in range(2, sheet.max_row + 1):
@@ -69,20 +76,6 @@ def mock_keyword_extraction(a):
     while i < length:
         for r in (libInfo):
             if list1[i].lower() == r:
-                ans_list.append({r: libraries[r]})
-        i = i + 1
-    return ans_list
-
-
-def read_from_json_and_test(a):
-    list = a.split(' ')
-    i = 0
-    ans_list = []
-
-    length = len(list)
-    while i < length:
-        for r in (libraries):
-            if list[i].lower() in r:
                 ans_list.append({r: libraries[r]})
         i = i + 1
     return ans_list
