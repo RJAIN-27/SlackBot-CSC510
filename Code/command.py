00784144a54671c68c9992d766903d94e272dd73
rjain27 <rjain27@ncsu.edu>
2019-10-21 21:06:57 -0400

    after an alternate flow

--- Code/command.py
+++ Code/command.py
@@ -10,17 +10,20 @@ class Command(object):
     def handlecommand(self, user, command):
         if "know" in command:
             list=kwx.keywordExtraction(command)
-            #list = command.split(' ')
+            print (list)
+            #list = command.split(' ') 
             return list
+        elif "Pssst" in command:
+            return "error"    
         elif "no" in command:
             return "Sorry about that :("
         elif "yes" in command:
             return "Thankyou for the feedback"
         elif flag==1:
-            analyses_file=analysis.analysis(path, command)
+            analyses_file=analysis.analysis(path, command) #analysis.analysisInteraction()
             return analyses_file
-        else:
-            model_Selection=modelSelection.modelSelection(path, command)   
+        elif flag==0:
+            model_Selection=modelSelection.modelSelection(path, command) #modelSelInteraction()
             return model_Selection
 
     def handlecommands(self, user, command):
