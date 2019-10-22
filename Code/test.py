32259f34c3e035ac5f1eedaa109d21e596cf77fa
skundal <skundal@ncsu.edu>
2019-10-22 12:54:07 -0400

    Update test.py

--- Code/test.py
+++ Code/test.py
@@ -19,6 +19,7 @@ wrong_target = data["wrong_target"]
 path_to_csv_file_case1_case2 = data["path_to_csv_file_case1_case2"]
 filename = data["path_for_usecase2"]
 
+
 # USECASE 1
 def mockbestModel(path, target):
     # for mocking the main and alternate flow
@@ -28,15 +29,18 @@ def mockbestModel(path, target):
     sorted_x = sorted(modelDict.items(), key=operator.itemgetter(1))
     return (sorted_x[len(sorted_x) - 1][0])
 
+
 def max_val_fun():
     ls = []
     for i in modelDict:
         ls.append(modelDict[i])
     return max(ls)
 
+
 # USECASE 1 & 2
 def targetCheck(target, columnNames):
-    return 1 if target in columnNames else "The target column is not present in the file. Please upload the file again and give the correct target column name. Remember, target column is case sensitive."
+    return 1 if target in columnNames else wrngColEx
+
 
 # USECASE 2
 def mock_analysis_interaction(path, target):
@@ -46,7 +50,8 @@ def mock_analysis_interaction(path, target):
 
     a = ""
     count = 0
-    for line in open(filename, 'r'):
+    f = open(filename,'r')
+    for line in f:
         a = a + line
     if "MEAN" in a:
         count = count + 1
@@ -58,9 +63,11 @@ def mock_analysis_interaction(path, target):
         count = count + 1
     if "Normality Tests" in a:
         count = count + 1
+    f.close()
     if count == parameters_to_be_counted:
         return filename
     return 0
+
 
 # USECASE 3
 def mock_keyword_extraction(a):
@@ -68,7 +75,7 @@ def mock_keyword_extraction(a):
     ans_list = []
     length = len(list)
     i = 0
-    wb = xl.load_workbook('libraryFile.xlsx')
+    wb = xl.load_workbook(data["xlsx_file"])
     sheet = wb['Sheet1']
     libInfo = {}
     for row in range(2, sheet.max_row + 1):
@@ -79,6 +86,7 @@ def mock_keyword_extraction(a):
                 ans_list.append({r: libraries[r]})
         i = i + 1
     return ans_list
+
 
 def read_from_json_and_test(a):
     list = a.split(' ')
@@ -93,11 +101,13 @@ def read_from_json_and_test(a):
         i = i + 1
     return ans_list
 
+
 class TestStringMethods(unittest.TestCase):
     # usecase 1 - happy flow
     @patch('modelSelection.modelSelInteraction', side_effect=mockbestModel)
     def test_modelsel(self, modelSelInteraction):
         self.assertEqual(modelDict[modelSelInteraction(path_to_csv_file_case1_case2, target)], max_val_fun())
+
     # usecase 1 - alternate flow
     @patch('modelSelection.modelSelInteraction', side_effect=mockbestModel)
     def test_modelsel2(self, modelSelInteraction):
@@ -107,19 +117,22 @@ class TestStringMethods(unittest.TestCase):
     @patch('analysis.analysisInteraction', side_effect=mock_analysis_interaction)
     def test_analysis1(self, analysisInteraction):
         self.assertEqual(analysisInteraction(path_to_csv_file_case1_case2, target), filename)
+
     # usecase2 - alternate flow
     @patch('analysis.analysisInteraction', side_effect=mock_analysis_interaction)
     def test_analysis2(self, analysisInteraction):
         self.assertEqual(analysisInteraction(path_to_csv_file_case1_case2, wrong_target), wrngColEx)
-    
+
     # usecase 3 - happy flow
     @patch('KeywordExtraction.keywordExtraction', side_effect=mock_keyword_extraction)
     def test_strings_a(self, keywordExtraction):
         self.assertEqual(keywordExtraction("know numpy pandas"), read_from_json_and_test("know numpy pandas"))
+
     # usecase 3 - alternate flow
     @patch('KeywordExtraction.keywordExtraction', side_effect=mock_keyword_extraction)
     def test_strings_b(self, keywordExtraction):
         self.assertEqual(keywordExtraction("jon snow knows nothing"), [])
+
 
 if __name__ == '__main__':
     unittest.main()
