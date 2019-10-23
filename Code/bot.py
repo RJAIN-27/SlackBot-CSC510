19f4c70c79a3d02112993916e1dd23508c9fbaa7
rjain27 <rjain27@ncsu.edu>
2019-10-22 23:39:16 -0400

    corrected the return for botid

--- Code/bot.py
+++ Code/bot.py
@@ -2,16 +2,18 @@ import time
 import event
 import command
 from slackclient import SlackClient
+import os
+
+global BOT_ID
 
 class Bot(object):
     def __init__(self):
-        self.slack_client = SlackClient("xoxb-795814705207-788531806065-9dWeyIRqj2t1LSbICYnDkB01")
+        #self.slack_client = SlackClient("xoxb-795814705207-788531806065-9dWeyIRqj2t1LSbICYnDkB01")
+        self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
         self.botname = "libbot"
         self.botid = self.getbotid()
-         
         if self.botid is None:
             exit("Error, could not find " + self.botname)
-     
         self.event = event.Event(self)
         self.command=command.Command()
 
@@ -23,7 +25,8 @@ class Bot(object):
             users = api_call.get('members')
             for user in users:
                 if 'name' in user and user.get('name') == self.botname:
-                    return "<@" + user.get('id') + ">"
+                    BOT_ID=(user.get('id'))
+                    return  user.get('id') 
             return None
              
     def listen(self):
