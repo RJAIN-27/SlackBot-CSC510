b19b436a85ad46ec66b6779aea860b2638413898
rjain27 <rjain27@ncsu.edu>
2019-10-22 22:53:54 -0400

    updated

--- Code/command.py
+++ Code/command.py
@@ -7,6 +7,7 @@ import unittest
 import test
 from mock import Mock
 
+
 with open("data.json") as json_file:
     jsonData = json.load(json_file)
 
@@ -16,6 +17,7 @@ flag=0
 KeywordExtraction.keywordExtraction = Mock(side_effect=mocking_infrastructure.mock_keyword_extraction)
 modelSelection.modelSelInteraction = Mock(side_effect=mocking_infrastructure.mockbestModel)
 analysis.analysisInteraction = Mock(side_effect = mocking_infrastructure.mock_analysis_interaction)
+
 
 class Command(object):
    
@@ -31,7 +33,7 @@ class Command(object):
             return "Sorry about that :("
         elif "yes" in command:
             return "Thankyou for the feedback"
-        elif flag==0:
+        elif flag==2:
             model_Selection=modelSelection.modelSelInteraction(path, command)
             #model_Selection=mocking_infrastructure.mockbestModel(path,command)
             return model_Selection
@@ -39,11 +41,13 @@ class Command(object):
             analysis_file=analysis.analysisInteraction(path, command)
             #analysis_file=mocking_infrastructure.mock_analysis_interaction(path,command)
             return analysis_file
+        elif flag==0:
+             return "Sorry I did not get you"
         
     def handlecommands(self, user, command):
         global flag
         if any(word in command for word in jsonData["model_sel_words"]):
-            flag=0
+            flag=2
             response="Please provide the target column"
             return response
 
