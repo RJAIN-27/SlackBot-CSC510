46b1056982d746b703b036db47e6a3a946138e8a
rjain27 <rjain27@ncsu.edu>
2019-10-22 22:55:45 -0400

    updated

--- Code/test.py
+++ Code/test.py
@@ -36,7 +36,7 @@ def max_val_fun():
 
 # USECASE 1 & 2
 def targetCheck(target, columnNames):
-    return 1 if target in columnNames else wrngColEx
+    return 1 if target in columnNames else "The target column is not present in the file. Please upload the file again and give the correct target column name. Remember, target column is case sensitive."
 
 # USECASE 2
 def mock_analysis_interaction(path, target):
@@ -68,7 +68,7 @@ def mock_keyword_extraction(a):
     ans_list = []
     length = len(list)
     i = 0
-    wb = xl.load_workbook(data["xlsx_file"])
+    wb = xl.load_workbook('libraryFile.xlsx')
     sheet = wb['Sheet1']
     libInfo = {}
     for row in range(2, sheet.max_row + 1):
