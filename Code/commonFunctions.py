a1d159bcdbe1d24839a00659649a9a20dc1789b6
skundal <skundal@ncsu.edu>
2019-10-26 00:47:26 -0400

    Update commonFunctions.py

--- Code/commonFunctions.py
+++ Code/commonFunctions.py
@@ -5,3 +5,9 @@ with open("data.json") as json_file:
 
 def targetCheck(target, columnNames):
     return 1 if target in columnNames else data["wrongTargetColumnException"]
+
+def checkAndConvertIfCategorical(df,target):
+    '''
+    convert categorical to numerical
+    '''
+    return df
