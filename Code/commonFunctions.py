5d0861e2ecb355a83a5da8f50d505214346ff1a1
skundal <skundal@ncsu.edu>
2019-10-26 01:07:25 -0400

    Modularizing common functions of usecase 1 and 2

--- Code/commonFunctions.py
+++ Code/commonFunctions.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 with open("data.json") as json_file:
     data = json.load(json_file)
@@ -11,3 +12,13 @@ def checkAndConvertIfCategorical(df,target):
     convert categorical to numerical
     '''
     return df
+
+def preprocessS1(path,target):
+    data = pd.read_csv(path, sep=',', header=0)
+    data = data[[col for col in data if data[col].nunique() > 1]]
+    column_names = list(data.columns)
+    flag = targetCheck(target, column_names)
+    if flag != 1:
+        return flag,[]
+    data = checkAndConvertIfCategorical(data, target)
+    return data,column_names
