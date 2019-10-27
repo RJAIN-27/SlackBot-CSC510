e17871124a5144cfca852872481b1484771a5a3a
vnukala2 <vnukala2@ncsu.edu>
2019-10-27 15:02:05 -0400

    updated working service of usecase3

--- Code/command.py
+++ Code/command.py
@@ -7,12 +7,12 @@ import unittest
 import test
 from mock import Mock
 
-
 with open("data.json") as json_file:
     jsonData = json.load(json_file)
 
 path = "my.csv"
-flag=0
+flag = 0
+
 
 # KeywordExtraction.keywordExtraction = Mock(side_effect=mocking_infrastructure.mock_keyword_extraction)
 # modelSelection.modelSelInteraction = Mock(side_effect=mocking_infrastructure.mockbestModel)
@@ -20,38 +20,38 @@ flag=0
 
 
 class Command(object):
-   
+
     def handlecommand(self, user, command):
         if "know" in command:
-            list=KeywordExtraction.keywordExtraction(command)
-            #list=mocking_infrastructure.mock_keyword_extraction(command)
-            print (list) 
+            list = KeywordExtraction.keywordExtraction(command)
+            # list=mocking_infrastructure.mock_keyword_extraction(command)
+            print (list)
             return list
         elif "Pssst" in command:
-            return "error"    
+            return "error"
         elif "no" in command:
             return "Sorry about that :("
         elif "yes" in command:
-            return "Thankyou for the feedback"
-        elif flag==2:
-            model_Selection=modelSelection.modelSelInteraction(path, command)
-            #model_Selection=mocking_infrastructure.mockbestModel(path,command)
+            return "Thank you for the feedback"
+        elif flag == 2:
+            model_Selection = modelSelection.modelSelInteraction(path, command)
+            # model_Selection=mocking_infrastructure.mockbestModel(path,command)
             return model_Selection
-        elif flag==1:
-            analysis_file=analysis.analysisInteraction(path, command)
-            #analysis_file=mocking_infrastructure.mock_analysis_interaction(path,command)
+        elif flag == 1:
+            analysis_file = analysis.analysisInteraction(path, command)
+            # analysis_file=mocking_infrastructure.mock_analysis_interaction(path,command)
             return analysis_file
-        elif flag==0:
-             return "Sorry I did not get you"
-        
+        elif flag == 0:
+            return "Sorry I did not get you"
+
     def handlecommands(self, user, command):
         global flag
         if any(word in command for word in jsonData["model_sel_words"]):
-            flag=2
-            response="Please provide the target column"
+            flag = 2
+            response = "Please provide the target column"
             return response
 
         if any(word in command for word in jsonData["analysis_words"]):
-            flag=1
-            response="Please provide the target column"
+            flag = 1
+            response = "Please provide the target column"
             return response
