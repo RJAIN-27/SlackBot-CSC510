9142305e3ec3543c5de95dc6d09d668346cb8b8c
skundal <skundal@ncsu.edu>
2019-11-02 16:28:07 -0400

    Update modelTraining.py

--- Code/modelTraining.py
+++ Code/modelTraining.py
@@ -18,10 +18,10 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = lsvc.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["Linear Support Vector Classifier"] = model_accr
-        f.writelines("\n      Accuracy of Linear Support Vector Classifier is " + str(model_accr))
+        f.writelines("\n            Accuracy of Linear Support Vector Classifier is " + str(model_accr))
     except:
         logging.info("LSVC is throwing exception")
-        f.writelines("\n      LSVC is throwing exception")
+        f.writelines("\n            LSVC is throwing exception")
 
     # KNN
     try:
@@ -29,10 +29,10 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = knn.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["KNN Classifier"] = model_accr
-        f.writelines("\n      Accuracy of KNN Classifier is " + str(model_accr))
+        f.writelines("\n            Accuracy of KNN Classifier is " + str(model_accr))
     except:
         logging.info("KNN is throwing exception")
-        f.writelines("\n      KNN is throwing exception")
+        f.writelines("\n            KNN is throwing exception")
 
     # DTC
     try:
@@ -40,20 +40,20 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = clf_gini.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["Decision Tree Classifier - GINI"] = model_accr
-        f.writelines("\n      Accuracy of Decision Tree Classifier - GINI is " + str(model_accr))
+        f.writelines("\n            Accuracy of Decision Tree Classifier - GINI is " + str(model_accr))
     except:
         logging.info("DTC GINI is throwing exception")
-        f.writelines("\n      DTC GINI is throwing exception")
+        f.writelines("\n            DTC GINI is throwing exception")
 
     try:
         clf_entropy = DecisionTreeClassifier(criterion="entropy", random_state=0)
         y_pred = clf_entropy.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["Decision Tree Classifier - ENTROPY"] = model_accr
-        f.writelines("\n      Accuracy of Decision Tree Classifier - ENTROPY is " + str(model_accr))
+        f.writelines("\n            Accuracy of Decision Tree Classifier - ENTROPY is " + str(model_accr))
     except:
         logging.info("DTC ENTROPY is throwing exception")
-        f.writelines("\n      DTC ENTROPY is throwing exception")
+        f.writelines("\n            DTC ENTROPY is throwing exception")
 
     # Multinomial NB
     try:
@@ -61,10 +61,10 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = mnb_model.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["Multinomial Naive Bayes"] = model_accr
-        f.writelines("\n      Accuracy of Multinomial NB is " + str(model_accr))
+        f.writelines("\n            Accuracy of Multinomial NB is " + str(model_accr))
     except:
         logging.info("Multinomial NB is throwing exception")
-        f.writelines("\n      Multinomial NB is throwing exception")
+        f.writelines("\n            Multinomial NB is throwing exception")
 
     # Bernoulli NB
     try:
@@ -72,10 +72,10 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = bnb_model.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["Bernoulli Naive Bayes"] = model_accr
-        f.writelines("\n      Accuracy of Bernoulli NB is " + str(model_accr))
+        f.writelines("\n            Accuracy of Bernoulli NB is " + str(model_accr))
     except:
         logging.info("Bernoulli NB is throwing exception")
-        f.writelines("\n      Bernoulli NB is throwing exception")
+        f.writelines("\n            Bernoulli NB is throwing exception")
 
     # Gaussian NB
     try:
@@ -83,10 +83,10 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = gnb_model.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["Gaussian Naive Bayes"] = model_accr
-        f.writelines("\n      Accuracy of GaussianNB is " + str(model_accr))
+        f.writelines("\n            Accuracy of GaussianNB is " + str(model_accr))
     except:
         logging.info("GaussianNB is throwing exception")
-        f.writelines("\n      GaussianNB is throwing exception")
+        f.writelines("\n            GaussianNB is throwing exception")
 
     # ADB
     try:
@@ -95,10 +95,10 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = adb.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["AdaBoost Classifier"] = model_accr
-        f.writelines("\n      Accuracy of AdaBoost Classifier is " + str(model_accr))
+        f.writelines("\n            Accuracy of AdaBoost Classifier is " + str(model_accr))
     except:
         logging.info("AdaBoost Classifier is throwing exception")
-        f.writelines("\n      AdaBoost Classifier is throwing exception")
+        f.writelines("\n            AdaBoost Classifier is throwing exception")
 
     # XGB
     try:
@@ -106,10 +106,10 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = xgb.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["XGB Classifier"] = model_accr
-        f.writelines("\n      Accuracy of XGB Classifier is " + str(model_accr))
+        f.writelines("\n            Accuracy of XGB Classifier is " + str(model_accr))
     except:
         logging.info("XGB Classifier is throwing exception")
-        f.writelines("\n      XGB Classifier is throwing exception")
+        f.writelines("\n            XGB Classifier is throwing exception")
 
     # Random Forest Classifier
     try:
@@ -117,9 +117,9 @@ def modelTraining(X_train, X_test, y_train, y_test,f):
         y_pred = rfc.fit(X_train, y_train).predict(X_test)
         model_accr = metrics.accuracy_score(y_test, y_pred) * 100
         models["Random Forest Classifier"] = model_accr
-        f.writelines("\n      Accuracy of Random Forest Classifier is " + str(model_accr))
+        f.writelines("\n            Accuracy of Random Forest Classifier is " + str(model_accr))
     except:
         logging.info("Random Forest Classifier is throwing exception")
-        f.writelines("\n      Random Forest Classifier is throwing exception")
+        f.writelines("\n            Random Forest Classifier is throwing exception")
 
     return (models)
