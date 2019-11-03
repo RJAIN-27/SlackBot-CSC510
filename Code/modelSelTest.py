06672b24db031006453e596cfe664d621feccd78
skundal <skundal@ncsu.edu>
2019-11-03 02:35:31 -0500

    Revert to previous file

--- Code/modelSelTest.py
+++ Code/modelSelTest.py
@@ -1,31 +1,51 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-import modelTraining as mt
-import commonFunctions as cf
+# Python code to demonstrate working of unittest using mock
+import operator
+import unittest
+import json
+import modelSelection
 
-def ngram(data, target, f):
-    cols = list(data.columns)
-    tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2),
-                            stop_words='english')
-    col_mod_dict = {}
-    for col in cols:
+with open("data.json") as json_file:
+    data = json.load(json_file)
+libraries = data["libraries"]
+columnNames = data["columnNames"]
+modelDict = data["listModels"]
+wrngColEx = data["wrongTargetColumnException"]
+parameters_to_be_counted = int(data["parameters_to_be_counted"])
+target = data["target"]
+wrong_target = data["wrong_target"]
+path_to_csv_file_case1_case2 = data["path_to_csv_file_case1_case2"]
+filename = data["path_for_usecase2"]
 
-        if not(data[col].dtypes=='float64' or data[col].dtypes=='int64') and col!=target:
-            try:
-                features = tfidf.fit_transform(data[col]).toarray()
-                X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, data[target], data.index, test_size=0.2,
-                                                                                                 random_state=0)
-                f.writelines("\n        Model accuracies when ngram based classification is performed on column " + str(col))
-                models = mt.modelTraining(X_train, X_test, y_train, y_test,f)
-                f.writelines("\n        Best model when ngram based classification is performed on column " + str(col))
-                best = cf.bestModel(models,f)
+# USECASE 1
+def best_models():
+    ls = []
+    accr = 0
+    for models in modelDict:
+        if accr<modelDict[models]:
+            accr = modelDict[models]
+    for model in modelDict:
+        if accr == modelDict[model]:
+            ls.append(model)
+    return ls
 
-                col_mod_dict[col]=best
-                for model in best:
-                    f.writelines("\n            "+model)
-                f.writelines("\n")
+class TestStringMethods(unittest.TestCase):
+    # usecase 1 - happy flow
+    def test_modelsel(self):
+        ls = best_models()
+        bestMod = modelSelection.modelSelInteraction(path_to_csv_file_case1_case2, target)
+        flag = 0
+        for model in ls:
+            if model not in bestMod:
+                flag = 1
+                break
+        self.assertEquals(flag,0) and self.assertEquals(len(ls),len(bestMod))
 
-            except Exception as e:
-                print(e)
-                f.writelines("\n        Cannot transform  " +str(col ) +" to array.")
-    return col_mod_dict
+    # usecase 1 - alternate flow
+    def test_modelsel2(self):
+       self.assertEqual(modelSelection.modelSelInteraction(path_to_csv_file_case1_case2, wrong_target), wrngColEx)
+
+    def test_modelsel3(self):
+        self.assertIsNotNone(modelSelection.modelSelInteraction("Crime1.csv","Category"))
+
+if __name__ == '__main__':
+    unittest.main()
