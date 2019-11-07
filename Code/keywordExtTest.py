140b36a3111445ff2da461aaea06963435b02d7f
skundal <skundal@ncsu.edu>
2019-11-07 16:38:47 -0500

    update tests

---
+++ Code/keywordExtTest.py
@@ -0,0 +1,20 @@
+# Python code to demonstrate working of unittest
+import unittest
+import json
+import KeywordExtraction as ke
+
+with open("data.json") as json_file:
+    data = json.load(json_file)
+
+libraries = data["libraries"]
+
+class TestKEMethods(unittest.TestCase):
+    # usecase 1 - happy flow
+    def test_KELib1(self):
+        self.assertIsNotNone(ke.keywordExtraction("I wanna know about numpy, scipy, keras and someLib","Sheet1"))
+
+    def test_KEFun1(self):
+        self.assertIsNotNone(ke.keywordExtraction("I wanna know about sin, cos, and tan","Sheet3"))
+
+if __name__ == '__main__':
+    unittest.main()
--- Code/modelSelTest.py
+++ Code/modelSelTest.py
@@ -1,4 +1,4 @@
-# Python code to demonstrate working of unittest using mock
+# Python code to demonstrate working of unittest
 import unittest
 import json
 import modelSelection
@@ -7,7 +7,7 @@ import pandas as pd
 
 with open("data.json") as json_file:
     data = json.load(json_file)
-    
+
 libraries = data["libraries"]
 columnNames = data["columnNames"]
 modelDict = data["listModels"]
@@ -16,17 +16,19 @@ parameters_to_be_counted = int(data["parameters_to_be_counted"])
 target = data["target"]
 wrong_target = data["wrong_target"]
 
+
 # USECASE 1
 def best_models():
     ls = []
     accr = 0
     for models in modelDict:
-        if accr<modelDict[models]:
+        if accr < modelDict[models]:
             accr = modelDict[models]
     for model in modelDict:
         if accr == modelDict[model]:
             ls.append(model)
     return ls
+
 
 class TestStringMethods(unittest.TestCase):
     # usecase 1 - happy flow
@@ -38,27 +40,28 @@ class TestStringMethods(unittest.TestCase):
             if model not in bestMod:
                 flag = 1
                 break
-        self.assertEquals(flag,0) and self.assertEquals(len(ls),len(bestMod))
+        self.assertEquals(flag, 0) and self.assertEquals(len(ls), len(bestMod))
 
     # usecase 1 - alternate flow
     def test_modelsel2(self):
-       self.assertEqual(modelSelection.modelSelInteraction("Datasets/Wine.csv", wrong_target), wrngColEx)
+        self.assertEqual(modelSelection.modelSelInteraction("Datasets/Wine.csv", wrong_target), wrngColEx)
 
     def test_modelsel3(self):
-        self.assertIsNotNone(modelSelection.modelSelInteraction("Datasets/Crime1.csv","Category"))
+        self.assertIsNotNone(modelSelection.modelSelInteraction("Datasets/Crime1.csv", "Category"))
 
     def test_categorical(self):
         data = pd.read_csv("Datasets/Crime1.csv", sep=',', header=0)
-        df,newcols,flag = cf.checkAndConvertIfCategorical(data,"Category")
-        cflag=0
+        df, newcols, flag = cf.checkAndConvertIfCategorical(data, "Category")
+        cflag = 0
         for col in newcols:
             if not (df[col].dtypes == 'float64' or df[col].dtypes == 'int64'):
                 cflag = 1
                 break
-        self.assertEquals(flag,1) and self.assertEquals(cflag,0)
+        self.assertEquals(flag, 1) and self.assertEquals(cflag, 0)
 
     def test_fileCreation(self):
-        self.assertIsNotNone(open("modelSelectionProcess.txt","r"))
+        self.assertIsNotNone(open("modelSelectionProcess.txt", "r"))
+
 
 if __name__ == '__main__':
     unittest.main()
