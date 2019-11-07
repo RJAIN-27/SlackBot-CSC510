b408bd7417f3e363df2e2fa92c9b617ffd904873
rjain27 <rjain27@ncsu.edu>
2019-11-07 16:57:10 -0500

    Final test cases for use case 1 and use case 2

--- Code/test.py
+++ Code/test.py
@@ -1,14 +1,14 @@
-# Python code to demonstrate working of unittest using mock
-import operator
 import unittest
-from mock import patch
 import json
-import openpyxl as xl
-import KeywordExtraction
 import modelSelection
+import commonFunctions as cf
+import pandas as pd
+import analysis
+
 
 with open("data.json") as json_file:
     data = json.load(json_file)
+
 libraries = data["libraries"]
 columnNames = data["columnNames"]
 modelDict = data["listModels"]
@@ -16,111 +16,117 @@ wrngColEx = data["wrongTargetColumnException"]
 parameters_to_be_counted = int(data["parameters_to_be_counted"])
 target = data["target"]
 wrong_target = data["wrong_target"]
-path_to_csv_file_case1_case2 = data["path_to_csv_file_case1_case2"]
-filename = data["path_for_usecase2"]
 
-# USECASE 1
-def mockbestModel(path, target):
-    # for mocking the main and alternate flow
-    flag = targetCheck(target, columnNames)
-    if flag != 1:
-        return flag
-    sorted_x = sorted(modelDict.items(), key=operator.itemgetter(1))
-    return (sorted_x[len(sorted_x) - 1][0])
+parameter_count_categorical = 5
+parameter_count_numerical = 4
 
-def max_val_fun():
+def best_models():
     ls = []
-    for i in modelDict:
-        ls.append(modelDict[i])
-    return max(ls)
+    accr = 0
+    for models in modelDict:
+        if accr<modelDict[models]:
+            accr = modelDict[models]
+    for model in modelDict:
+        if accr == modelDict[model]:
+            ls.append(model)
+    return ls
 
-# USECASE 1 & 2
-def targetCheck(target, columnNames):
-    return 1 if target in columnNames else wrngColEx
-
-# USECASE 2
-def mock_analysis_interaction(path, target):
-    flag = targetCheck(target, columnNames)
-    if flag != 1:
-        return flag
-
+def contentCheck_categorical():
+    filename = "Analysis.txt"
     a = ""
     count = 0
-    f = open(filename,'r')
-    for line in f:
+    for line in open(filename, 'r'):
         a = a + line
-    if "MEAN" in a:
+    if "VALUE COUNTS" in a:
         count = count + 1
-    if "MEDIAN" in a:
+    if "DATA INFORMATION" in a:
         count = count + 1
-    if "MODE" in a:
+    if "MEAN, MEDIAN AND MODE:" in a:
         count = count + 1
     if "Correlation" in a:
         count = count + 1
     if "Normality Tests" in a:
         count = count + 1
-    if count == parameters_to_be_counted:
-        return filename
-    return 0
+    return count 
 
-# USECASE 3
-def mock_keyword_extraction(a):
-    list = a.split(' ')
-    ans_list = []
-    length = len(list)
-    i = 0
-    wb = xl.load_workbook(data["xlsx_file"])
-    sheet = wb['Sheet1']
-    libInfo = {}
-    for row in range(2, sheet.max_row + 1):
-        libInfo[sheet.cell(row, 1).value] = sheet.cell(row, 2).value
-    while i < length:
-        for r in (libInfo):
-            if list[i].lower() in r:
-                ans_list.append({r: libraries[r]})
-        i = i + 1
-    return ans_list
-
-def read_from_json_and_test(a):
-    list = a.split(' ')
-    i = 0
-    ans_list = []
-
-    length = len(list)
-    while i < length:
-        for r in (libraries):
-            if list[i].lower() == r:
-                ans_list.append({r: libraries[r]})
-        i = i + 1
-    return ans_list
-
+def contentCheck_numerical():
+    filename = "Analysis.txt"
+    a = ""
+    count = 0
+    for line in open(filename, 'r'):
+        a = a + line
+    if "DATA INFORMATION" in a:
+        count = count + 1
+    if "MEAN, MEDIAN AND MODE:" in a:
+        count = count + 1
+    if "Correlation" in a:
+        count = count + 1
+    if "Normality Tests" in a:
+        count = count + 1
+    return count 
+    
 class TestStringMethods(unittest.TestCase):
     # usecase 1 - happy flow
-    @patch('modelSelection.modelSelInteraction', side_effect=mockbestModel)
-    def test_modelsel(self, modelSelInteraction):
-        self.assertEqual(modelDict[modelSelInteraction(path_to_csv_file_case1_case2, target)], max_val_fun())
-    # usecase 1 - alternate flow
-    @patch('modelSelection.modelSelInteraction', side_effect=mockbestModel)
-    def test_modelsel2(self, modelSelInteraction):
-        self.assertEqual(modelSelInteraction(path_to_csv_file_case1_case2, wrong_target), wrngColEx)
+    def test_modelsel(self):
+        ls = best_models()
+        bestMod = modelSelection.modelSelInteraction("Datasets/Wine.csv", target)
+        flag = 0
+        for model in ls:
+            if model not in bestMod:
+                flag = 1
+                break
+        self.assertEquals(flag,0) and self.assertEquals(len(ls),len(bestMod))
 
-    # usecase2 - happy flow
-    @patch('analysis.analysisInteraction', side_effect=mock_analysis_interaction)
-    def test_analysis1(self, analysisInteraction):
-        self.assertEqual(analysisInteraction(path_to_csv_file_case1_case2, target), filename)
-    # usecase2 - alternate flow
-    @patch('analysis.analysisInteraction', side_effect=mock_analysis_interaction)
-    def test_analysis2(self, analysisInteraction):
-        self.assertEqual(analysisInteraction(path_to_csv_file_case1_case2, wrong_target), wrngColEx)
+    # usecase 1 - alternate flow
+    def test_modelsel2(self):
+       self.assertEqual(modelSelection.modelSelInteraction("Datasets/Wine.csv", wrong_target), wrngColEx)
+
+    def test_modelsel3(self):
+        self.assertIsNotNone(modelSelection.modelSelInteraction("Datasets/Crime1.csv","Category"))
+
+    def test_categorical(self):
+        data = pd.read_csv("Datasets/Crime1.csv", sep=',', header=0)
+        df,newcols,flag = cf.checkAndConvertIfCategorical(data,"Category")
+        cflag=0
+        for col in newcols:
+            if not (df[col].dtypes == 'float64' or df[col].dtypes == 'int64'):
+                cflag = 1
+                break
+        self.assertEquals(flag,1) and self.assertEquals(cflag,0)
+
+    def test_fileCreation(self):
+        self.assertIsNotNone(open("modelSelectionProcess.txt","r"))
+
+
+    # Use case 2 happy flow 
+    def test_analysis_categorical(self):
+            analysis.analysisInteraction("Datasets/Crime1.csv","Category")
+            count_cat = contentCheck_categorical()
+            self.assertEquals(count_cat,parameter_count_categorical)
+            
+    def test_analysis_numerical(self):
+            analysis.analysisInteraction("Datasets/Wine.csv","Class")
+            count_cat = contentCheck_numerical()
+            self.assertEquals(count_cat,parameter_count_numerical)
+            
+    # usecase 2 - alternate flow
+    def test_analysis_target_cat(self):
+           self.assertEqual(analysis.analysisInteraction("Datasets/Crime1.csv", "Hululu"), wrngColEx)
     
-    # usecase 3 - happy flow
-    @patch('KeywordExtraction.keywordExtraction', side_effect=mock_keyword_extraction)
-    def test_strings_a(self, keywordExtraction):
-        self.assertEqual(keywordExtraction("know numpy pandas"), read_from_json_and_test("know numpy pandas"))
-    # usecase 3 - alternate flow
-    @patch('KeywordExtraction.keywordExtraction', side_effect=mock_keyword_extraction)
-    def test_strings_b(self, keywordExtraction):
-        self.assertEqual(keywordExtraction("jon snow knows nothing"), [])
+    def test_analysis_target_num(self):
+           self.assertEqual(analysis.analysisInteraction("Datasets/Wine.csv", "Hululu"), wrngColEx) 
+            
+    def test_analysis_result_cat(self):
+        self.assertIsNotNone(analysis.analysisInteraction("Datasets/Crime1.csv","Category"))
+        
+    def test_analysis_result_num(self):
+        self.assertIsNotNone(analysis.analysisInteraction("Datasets/Wine.csv","Class"))
+        
+    def test_fileCreation(self):
+        self.assertIsNotNone(open("Analysis.txt","r"))
+
 
 if __name__ == '__main__':
     unittest.main()
+        
+    
