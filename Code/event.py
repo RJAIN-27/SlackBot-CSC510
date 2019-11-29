6385833a039a115c61e3c8e996bf475c06940b32
rjain27 <rjain27@ncsu.edu>
2019-11-29 01:26:26 -0500

    Update event.py

--- Code/event.py
+++ Code/event.py
@@ -3,8 +3,11 @@ import command
 import requests
 import io
 import os
+import mocking_infrastructure
+from mock import Mock
 import bot
 import json
+
 
 with open("/home/CSC510-23/Code/data.json") as json_file:
     data = json.load(json_file)
@@ -26,31 +29,31 @@ class Event:
                 self.parseevent(event)
                  
     def parseevent(self, event):  
+        #print event
         if event and 'text' in event and 'files' not in event and event['user']!="UP6FMPQ1X" and event['user']!="USLACKBOT":   
             self.handleevent(event['user'], event['text'], event['channel'])
         elif event and 'files' in event and 'text' in event and event['user']!="UP6FMPQ1X" and event['upload']==True:
             if event['text'] == '':
                 self.bot.slack_client.api_call("chat.postMessage", channel=event['channel'], text="I am sorry can you please give me a csv file with the details of what is to be done\n", as_user=True)
             elif(event['files'][0]['filetype'] == "csv"):
-                #response = requests.get(event['files'][0]['url_private'], headers={'Authorization': 'Bearer xoxb-795814705207-788531806065-9dWeyIRqj2t1LSbICYnDkB01'})
                 auth = 'Bearer '+str(TOKEN)
                 response = requests.get(event['files'][0]['url_private'], headers={'Authorization': auth})
-                print response.content
+                #response = requests.get(event['files'][0]['url_private'], headers={'Authorization': 'Bearer xoxb-795814705207-788531806065-9dWeyIRqj2t1LSbICYnDkB01'})
+                #response = requests.get(event['files'][0]['url_private'], headers={'Authorization': 'Bearer TOKEN'})
+                #print response.content
                 with open("my.csv",'wb') as f: 
                     f.write(response.content) 
                 f.close()    
                 self.handleevent1(event['user'], event['text'], event['channel']) 
             else:
                 self.bot.slack_client.api_call("chat.postMessage", channel=event['channel'], text="I am sorry can you please give me a csv file\n", as_user=True)
-        #if event and 'files' in event and 'text' not in event and event['user']!="UP6FMPQ1X" and event['upload']==True:
 
     def rep(self, response, user, command, channel, flag):
-       
+        print response
         if type(response) is str:   
                 if(".txt" in response):
                     f1=open(response, "r")
                     content=f1.read()
-                    print(content)
                     f1.close()
                     self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="The analysis of your dataset is:\n", as_user=True)
                     self.bot.slack_client.api_call("files.upload", channels=channel, file=content, filename="analysis.txt")
@@ -76,11 +79,15 @@ class Event:
                     self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="Were you satisfied with the recommendation?", as_user=True)
 
         elif type(response) is list and len(response)!=0:  
-
-            if (type(response[0]) is dict):
+            if (type(response[0]) is str and response[0]=="onlylibrary" or response[0]=="onlyfunction" ):
+                response.pop()
+                print response
+                
+                self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="I am sorry, we are still working and building our database!", as_user=True)
+            elif (type(response[0]) is dict):
                 if (response[-1]=="onlylibrary"):
                     response.pop()
-                self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="The details of the libraries or functions you asked are:\n", as_user=True)
+                self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="The details of the libraries or libraries you asked are:\n", as_user=True)
                 if flag==0:
                     for i in response:
                         for j in i:
@@ -95,7 +102,7 @@ class Event:
                             # l_of_lib.append(j)
                     self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="Were you satisfied with the above provided information", as_user=True) 
                 #self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="Were you satisfied with the details?", as_user=True)
-            if (type(response[0]) is str):
+            elif (type(response[0]) is str):
                 self.bot.slack_client.api_call("chat.postMessage", channel=channel, text="The best models that you can use are:\n", as_user=True)
                 for i in response:
                     self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=i, as_user=True)
@@ -153,3 +160,5 @@ class Event:
             response = self.command.handlecommands(user, command)   
             self.bot.slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
             
+
+
